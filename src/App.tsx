6cedde8914805c35454a32c2adbba3a13ef4248d
GlitchRider1 <glitchrider831@gmail.com>
2025-07-04 19:15:22 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+// File: src/App.tsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Builder from './pages/Builder';
+import Templates from './pages/Templates';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/builder" element={<Builder />} />
+        <Route path="/templates" element={<Templates />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
